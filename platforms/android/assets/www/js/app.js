b97be3dbe237bde43aea28d19a7cf997d6ac80a5
Abhay Amin <pixoloproductions@gmail.com>
2015-08-26 03:18:30 +0530

    0.300 +- radius

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,119 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'myservices','uiGmapgoogle-maps'])/*,'google.places'*/
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+        .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.search', {
+        url: '/search',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/search.html',
+                controller: 'searchCtrl'
+            }
+        }
+    })
+
+    .state('app.account', {
+            url: '/account',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/account.html',
+                    controller: 'accountCtrl'
+                }
+            }
+        })
+        .state('app.share', {
+            url: '/share',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/share.html',
+                    controller: 'shareCtrl'
+                }
+            }
+        })
+        .state('app.signup', {
+            url: '/signup',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/signup.html',
+                    controller: 'signupCtrl'
+                }
+            }
+        })
+        .state('app.otp', {
+            url: '/otp',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/otp.html',
+                    controller: 'otpCtrl'
+                }
+            }
+        })
+        .state('app.login', {
+            url: '/login',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/login.html',
+                    controller: 'loginCtrl'
+                }
+            }
+        })
+        .state('app.vehiclelist', {
+            url: '/vehiclelist/:type',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/vehiclelist.html',
+                    controller: 'vehiclelistCtrl'
+                }
+            }
+        });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/search');
+})
+.directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {country: 'in'}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    model.$setViewValue(element.val());                
+                });
+            });
+        }
+    };
+});;
---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,272 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, $ionicNavBarDelegate) {
+
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
+
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+            $scope.closeLogin();
+        }, 1000);
+    };
+})
+
+.controller('shareCtrl', function ($scope, $stateParams) {})
+    .controller('accountCtrl', function ($scope, $stateParams) {})
+
+.controller('loginCtrl', function ($scope, $stateParams, $location) {
+
+        $scope.login = function () {
+            $location.path("/app/home");
+        };
+
+        $scope.opensignuppage = function () {
+            $location.path("/app/signup");
+        };
+    })
+    .controller('otpCtrl', function ($scope, $stateParams, $location) {
+        $scope.verifyotp = function () {
+            $location.path("/app/home");
+        };
+    })
+    .controller('signupCtrl', function ($scope, $stateParams, $location, MyServices) {
+
+        $scope.signupdata = {};
+
+        var signupsuccess = function (data, status) {
+            if (data == "false") {
+                alert("Number already registered");
+            } else {
+                $location.path("/app/otp");
+                console.log(data);
+            };
+        };
+
+        $scope.signup = function () {
+            MyServices.signupuser($scope.signupdata).success(signupsuccess);
+        };
+    })
+    .controller('searchCtrl', function ($scope, $stateParams, $location, $http) {
+
+
+        $scope.input = {};
+        $scope.input.placeinput;
+
+        $scope.gPlace;
+
+        $scope.vehicletypes = [{
+                id: 1,
+                selected: true,
+                icon: "icon-car",
+                image: "img/car_display_pic.png",
+                value: "tourist"
+    },
+            {
+                id: 2,
+                selected: false,
+                icon: "icon-truck",
+                image: "img/truck_display_pic.png",
+                value: "tourist"
+    },
+            {
+                id: 3,
+                selected: false,
+                icon: "icon-auto",
+                image: "img/rickshaw_display_pic.png",
+                value: "transporter"
+    },
+            {
+                id: 4,
+                selected: false,
+                icon: "icon-taxi",
+                image: "img/taxi_display_pic.png",
+                value: "transporter"
+    }];
+
+        $scope.showimage = $scope.vehicletypes[0].image;
+
+        $scope.selectvehicle = function (id) {
+            for (var q = 0; q < $scope.vehicletypes.length; q++) {
+                if (id == $scope.vehicletypes[q].id) {
+                    $scope.vehicletypes[q].selected = true;
+                    $scope.showimage = $scope.vehicletypes[q].image;
+                } else {
+                    $scope.vehicletypes[q].selected = false;
+                };
+            };
+        };
+
+
+
+        $scope.getvehicles = function (type) {
+            var input = $('#inp').val();
+            console.log(input);
+            $http.get("http://maps.google.com/maps/api/geocode/json", {
+                params: {
+                    address: input,
+                    sensor: false
+                }
+            }).success(function (data, status) {
+                console.log(data.results[0].geometry.location);
+                console.log(data.results[0].geometry.viewport.northeast);
+                console.log(data.results[0].geometry.viewport.southwest);
+                for (var w = 0; w < $scope.vehicletypes.length; w++) {
+                    if ($scope.vehicletypes[w].selected == true) {
+                        var type = $scope.vehicletypes[w].value
+                    };
+                };
+                $location.path("/app/vehiclelist/" + type);
+            });
+        };
+
+    })
+    .controller('vehiclelistCtrl', function ($scope, $stateParams, $location, MyServices, $ionicLoading, $ionicSideMenuDelegate) {
+
+        //PAGE SETUP
+        //CAN DRAG CONTENT FOR MENU - TRUE
+        $ionicSideMenuDelegate.canDragContent(true);
+        console.log($ionicSideMenuDelegate.canDragContent(true));
+        //TAB TO SHOW VARIABLE
+        $scope.tab = false;
+
+        var type = $stateParams.type;
+
+        $ionicLoading.show({
+            template: 'Fetching Vehicles...'
+        });
+
+        var getvehiclesbytypesuccess = function (data, status) {
+            console.log(data);
+            $scope.vehicledata = data;
+            $ionicLoading.hide();
+        };
+        var location = {
+            latitude: "19.50",
+            longitude: "23.50"
+        };
+        MyServices.getvehiclesbytype(type, location).success(getvehiclesbytypesuccess);
+
+        $scope.map = {
+            center: {
+                latitude: 18.9750,
+                longitude: 72.8258
+            },
+            zoom: 8
+        };
+
+        $scope.markers = [];
+
+        var marker1 = {
+            "id": 1,
+            "location": {
+                "latitude": 18.9750,
+                "longitude": 72.8258
+            },
+            "icon": "../img/rickshaw_display_pic.png"
+
+        };
+        var marker2 = {
+            "id": 2,
+            "location": {
+                "latitude": 18.2725,
+                "longitude": 72.9240
+            },
+            "icon": "../img/rickshaw_display_pic.png"
+        };
+
+        $scope.markers.push(marker1);
+        $scope.markers.push(marker2);
+
+        //CIRCLE
+        $scope.circles = [
+            {
+                id: 1,
+                center: {
+                    "latitude": 18.9725,
+                    "longitude": 72.8240
+                },
+                radius: 500000,
+                stroke: {
+                    color: '#08B21F',
+                    weight: 2,
+                    opacity: 1
+                },
+                fill: {
+                    color: '#08B21F',
+                    opacity: 0.5
+                },
+                geodesic: true, // optional: defaults to false
+                draggable: true, // optional: defaults to false
+                clickable: true, // optional: defaults to true
+                editable: true, // optional: defaults to false
+                visible: true, // optional: defaults to true
+                control: {}
+            }
+        ];
+
+        //RECTANGLE
+        $scope.bounds = {
+            ne: {
+                latitude: 18.9750,
+                longitude: 73.8258
+            },
+            sw: {
+                latitude: 19.9725,
+                longitude: 72.8240
+            }
+        };
+
+        $scope.disableTap = function () {
+            console.log("focus");
+            container = document.getElementsByClassName('pac-container');
+            // disable ionic data tab
+            angular.element(container).attr('data-tap-disabled', 'true');
+            // leave input field if google-address-entry is selected
+            angular.element(container).on("click", function () {
+                document.getElementById('inp').blur();
+            });
+        };
+
+        var input = document.getElementById('inp');
+        var options = { //options for autocomplete object
+            types: ['geocode']
+        };
+        autocomplete = new google.maps.places.Autocomplete(input, options);
+
+        $scope.boundsChanged = function (event) {
+            console.log("CHANGED")
+        };
+
+
+
+    });
