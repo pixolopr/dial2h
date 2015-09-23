ad7c36387defa09ec8be73423bec391fb72dc414
Abhay Amin <pixoloproductions@gmail.com>
2015-09-24 04:26:41 +0530

    removed platforms

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\xampp\htdocs\dial2hireapp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\xampp\htdocs\dial2hireapp\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\xampp\htdocs\dial2hireapp\platforms\android\ant-build\AndroidManifest.xml : \
-C:\xampp\htdocs\dial2hireapp\platforms\android\AndroidManifest.xml \
